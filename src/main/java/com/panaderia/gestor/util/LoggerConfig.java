3cf6f0cb61e2412b63d468d80a0882e49cbbd402
absanr <devprogramer@outlook.com>
2024-07-18 21:32:00 -0500

    se agrego log para la ruta recources

--- src/main/java/com/panaderia/gestor/util/LoggerConfig.java
+++ src/main/java/com/panaderia/gestor/util/LoggerConfig.java
@@ -2,16 +2,29 @@ package com.panaderia.gestor.util;
 
 import java.io.IOException;
 import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class LoggerConfig {
-    private static Logger logger = Logger.getLogger("PanaderiaLogger");
+    private static final Logger logger = Logger.getLogger(LoggerConfig.class.getName());
+    private static final String LOG_FILE_PATH = "src/main/resources/log.txt";
 
-    public static void setup() throws IOException {
-        FileHandler fh = new FileHandler("panaderia.log", true);
-        fh.setFormatter(new SimpleFormatter());
-        logger.addHandler(fh);
+    static {
+        try {
+            // Remove default console handler
+            Logger rootLogger = Logger.getLogger("");
+            rootLogger.setLevel(Level.OFF);
+
+            // Setup file handler
+            FileHandler fileHandler = new FileHandler(LOG_FILE_PATH, true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+
+            logger.setLevel(Level.ALL);
+        } catch (IOException e) {
+            System.err.println("Error al configurar el logger: " + e.getMessage());
+        }
     }
 
     public static Logger getLogger() {
